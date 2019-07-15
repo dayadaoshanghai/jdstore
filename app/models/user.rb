fbcf4c405f23fdf1eaa311f377701b36810af6e3
Johnson <974456843@qq.com>
2019-07-15 22:17:15 +0800

    add order controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  has_many: orders
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   def admin?
     is_admin
-  end 
+  end
 end
